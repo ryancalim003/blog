1d415cb73d870d361d442e3f5a6bd7d658fb42b8
Ryan <ryancalim003@gmail.com>
2022-04-18 04:31:39 +0800

    change tagline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Demo
+                    Demo Page
                 </div>
 
                 <div class="links">
